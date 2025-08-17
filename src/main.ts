cd9b6972faca720e51fad398fa4bae1549bf7dc4
YU <yuji.kasamatsu@gmail.com>
2025-08-17 22:50:51 +0900

    fix

--- src/main.ts
+++
@@ -1,9 +0,0 @@
-import { Game } from "./Lib/Game";
-import { InputMng } from "./Lib/InputMng";
-import { Point } from "./Lib/Point";
-
-document.addEventListener('DOMContentLoaded', (e) => {
-	Point.init(30, 30)
-	new Game()
-	new InputMng()
-});
